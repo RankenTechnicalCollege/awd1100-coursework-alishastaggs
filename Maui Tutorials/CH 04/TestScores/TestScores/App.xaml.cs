ea4464bfbaa54f84a33e41b258fc3c81bf763092
alishastaggs <alishastaggs175@gmail.com>
2025-06-12 01:08:49 -0500

    MAUI test score calc tut

---
+++ Maui Tutorials/CH 04/TestScores/TestScores/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace TestScores
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            window.Title = "My Score Calculator";
+            return window;
+        }
+    }
+}
