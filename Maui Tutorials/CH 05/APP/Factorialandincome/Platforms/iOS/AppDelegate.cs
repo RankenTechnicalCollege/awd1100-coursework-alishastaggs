3acd49710c014948c7db2c037bd6aae3e3794a57
alishastaggs <alishastaggs175@gmail.com>
2025-06-20 01:59:36 -0500

    MAUI tut Factorial Calc and Tax App

---
+++ Maui Tutorials/CH 05/APP/Factorialandincome/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Factorialandincome
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
